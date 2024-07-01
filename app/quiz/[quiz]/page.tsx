2474abd4a510498f11aa2c5340b31143e0fb7caa
rwinkah <rwinosa@gmail.com>
2024-07-01 23:02:05 +0000

    feat: update quz

--- app/quiz/[quiz]/page.tsx
+++ app/quiz/[quiz]/page.tsx
@@ -2,18 +2,17 @@
 import lessons from "@/data/course-info";
 
 
-export default function Page({ params }: { params: { lesson: string } }) {
-    let currentLesson = params.lesson;
+export default function Page({ params }: { params: { quiz: string } }) {
+    let currentLesson = params.quiz;
     const lessonData = lessons.find((lesson) => lesson.id === currentLesson);
-  
     if (!lessonData) {
       return <div className="text-primary text-4xl">Lesson unavailable</div>;
     }
 
     return (
-        <div className="bg-[#2a0b38]">
-            <div id="quiz-header">
-                <h1 className="text-primaryLight">{lessonData.title} </h1>
+        <div className="bg-[#2a0b38] w-full h-[100vh] overflow-scroll flex flex-col p-10">
+            <div id="quiz-header" className="self-center">
+                <h1 className="font-bold  text-4xl text-white"> <span className="text-primaryLight">Onboard</span>Me Quiz </h1>
             </div>
         </div>
     )
