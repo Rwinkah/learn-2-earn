f733bd2c3406f433d22bcefbe9357400b4e687c6
rwinkah <rwinosa@gmail.com>
2024-07-02 13:13:25 +0000

    fix: build optimization for v.0.1.0

--- app/quiz/[quiz]/page.tsx
+++ app/quiz/[quiz]/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import QuizCard from "@/app/_components/quiz-card";
 import lessons from "@/data/course-info";
 
 
@@ -10,9 +11,14 @@ export default function Page({ params }: { params: { quiz: string } }) {
     }
 
     return (
-        <div className="bg-[#2a0b38] w-full h-[100vh] overflow-scroll flex flex-col p-10">
+        <div className="bg-[#2a0b38] w-full h-[100vh] overflow-scroll flex flex-col p-10 gap-20 pt-10">
             <div id="quiz-header" className="self-center">
                 <h1 className="font-bold  text-4xl text-white"> <span className="text-primaryLight">Onboard</span>Me Quiz </h1>
+            </div>
+            <div className="flex items-center justify-center flex-col gap-14">
+                {lessonData.quiz.map((item, index)=> (
+                    <QuizCard key={index} {...item} />
+                ))}
             </div>
         </div>
     )
