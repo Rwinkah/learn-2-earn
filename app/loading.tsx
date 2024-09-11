28eb7a92b16d0f4c29505a1951804514704e9e2e
rwinkah <rwinosa@gmail.com>
2024-09-11 02:13:38 +0000

    fix: loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,6 @@ export default function Loading() {
 	return (
 		<div className="w-full flex items-center justify-center">
 			<div className="loading" />
-			<div className="text-white text-xl">loading</div>
 		</div>
 	);
 }
